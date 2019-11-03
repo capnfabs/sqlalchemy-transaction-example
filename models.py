2a11fa0d4184e2c1ed868ad346a3f788b2c71798
Fabian Tamp <fabian.tamp@gmail.com>
2019-11-03 21:44:51 +0100

    More changes

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, DateTime, Integer
+from sqlalchemy import Column, String, DateTime, Integer, Numeric
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -11,3 +11,14 @@ class Zebra(Base):
     when_born = Column(DateTime, nullable=False)
     when_stripes_counted = Column(DateTime)
     number_of_stripes = Column(Integer)
+
+    def __repr__(self):
+        return f"Zebra(name='{self.name}')"
+
+
+class WateringHole(Base):
+    __tablename__ = 'watering_holes'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    latitude = Column(Numeric(precision=6), nullable=False)
+    longitude = Column(Numeric(precision=6), nullable=False)
